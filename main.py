d339812be1f663fc36f1ecc82c85ceaf9a08c32f
Brian Jesus Nicolas Segovia <briansegovia2022@gmail.com>
2023-02-28 15:18:56 -0300

    Primer cambio

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+    # crea un programa donde pida el nombre del usuario 
+    # y lo devuelva con el siguiente mensaje 
+    # bienvenido {usuario} a la matrix 
+    
+    nombre: str 
+    
+    devolver un str 
+"""
+
+nombre = input('Proporcione tu Nombre: ')
+
+print(f'Bienvenido {nombre} a la matrix. ')
